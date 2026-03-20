7afc80eb996789197639b5e08c29f36066d6b774
Rajkumar Natarajan <rajkumar.natarajan.-nd@disney.com>
2026-03-20 16:43:28 -0700

    install: map rustc 1.84.x to FireDBG 1.84.0

--- install.sh
+++ install.sh
@@ -37,6 +37,9 @@ get_firedbg_version() {
         rustc\ 1.83.*)
             local _firedbg_version="1.83.0"
             ;;
+        rustc\ 1.84.*)
+            local _firedbg_version="1.84.0"
+            ;;
         *)
             err "no precompiled binaries available for $_rustc_version";
             ;;
